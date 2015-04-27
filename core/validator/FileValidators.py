143b30047951c6ff831eb5b46c818a9ab2b2a34c
Lubin Molano <lmolano@casewaresa.com>
2015-04-27 11:33:23 -0500

    Cambio de numeracion y creacion de archivos faltantes

--- core/validator/FileValidators.py
+++ core/validator/FileValidators.py
@@ -2,9 +2,15 @@
 
 from enum import Enum
 class FileValidators(Enum):
-    SEQUENCE = 6
     TABLE = 1
-    VISTA = 2
+    SEQUENCE = 2
     INDEX = 3
-    REPORTE = 4
-    CONSTRAINT = 5
+    PACKAGE = 4
+    PACKAGE_BODY = 5
+    CONSTRAINT = 6
+    NO_CONSTRAINT = 7
+    VIEW_BI = 8
+    VIEW_DATA = 9 
+    VIEW_JOIN =  10
+    VIEW_LOV = 11
+    VIEW_REPORT = 12
