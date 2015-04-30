965e01d4bad3835263ad34621e1034e3aefaa7e9
Lubin Molano <lmolano@casewaresa.com>
2015-04-30 17:48:03 -0500

    pruebas para encontrar nombres de archivo repetido

--- core/format/ConstraintForeingKeyFormatter.py
+++ core/format/ConstraintForeingKeyFormatter.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 import os
+import re
 class ConstraintForeingKeyFormatter:
 
 	def isForeingKeyDirectory(self,directory):
-		if 'constraint' in directory and 'nofk'in directory :
+		if 'constraint' in directory and 'fk'in directory :
 			return True
 		else:
 			return False 
@@ -11,8 +12,17 @@ class ConstraintForeingKeyFormatter:
 	def format(self, directory):
     	#afterline almacena el nombre del archivo anterior
     	#si son iguales se unen ambos archivos
+
 		AfterFile=''
+
 		if self.isForeingKeyDirectory(directory):
 			for fileList in os.walk(directory):
-				for fname in fileList:
-					print(fname.split("_"))
+				for files in fileList:
+					#for archivo in files:
+					print([m.group(1) for l in files for m in [re.search(l,files)] if m])
+					#src=regex.search; 
+					#lst=[m.group(1) for l in lines for m in [src(l)] if m]
+						#print( "----------------------\n")
+						#print( str(archivo)[:str(archivo).rfind("_")])
+						#print( "----------------------\n")
+						#print(str(fname).split("_"))
